d3f6a3609577fab6e2460c0e788338728ca7d13f
TobiasVdE <tobias.vdeynde@gmail.com>
2020-12-25 14:34:49 +0100

    Randomize colours

--- code.py
+++ code.py
@@ -5,17 +5,29 @@ import game
 trellis = adafruit_trellism4.TrellisM4Express()
 trellis.pixels.auto_write = False
 
+def ShowGame(g):
+    for column in range(8):
+        for row in range(4):
+            trellis.pixels[column, row] = g.cells[column][row].colour
+    trellis.pixels.show()        
+
 current_press = set()
 while True:
-    pressed = set(trellis.pressed_keys)
-    for press in pressed - current_press:
-        for x in range(8):
-            trellis.pixels[x, 0] = game.GREEN
-            trellis.pixels[x, 1] = game.BLUE
-            trellis.pixels[x, 2] = game.GREEN
-            trellis.pixels[x, 3] = game.BLUE
-    for release in current_press - pressed:
-        trellis.pixels.fill(game.OFF)
-    trellis.pixels.show()
-    time.sleep(0.08)
-    current_press = pressed
+    g = game.Game()
+    ShowGame(g)
+    
+    while True:
+        pass
+
+    # pressed = set(trellis.pressed_keys)
+    # for press in pressed - current_press:
+    #     for x in range(8):
+    #         trellis.pixels[x, 0] = game.GREEN
+    #         trellis.pixels[x, 1] = game.BLUE
+    #         trellis.pixels[x, 2] = game.RED
+    #         trellis.pixels[x, 3] = game.YELLOW
+    # for release in current_press - pressed:
+    #     trellis.pixels.fill(game.OFF)
+    # trellis.pixels.show()
+    # time.sleep(0.08)
+    # current_press = pressed
--- game.py
+++ game.py
@@ -1,12 +1,22 @@
+import random
+
 from cell import Cell
 
 RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
+YELLOW = (150, 125, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 OFF = (0, 0, 0)
 
 class Game:
+    def randomize(self):
+        for index in range(8):
+            column = self.cells[index]
+            for iteration in range(10):
+                first = random.randrange(4)
+                second = random.randrange(4)
+                column[first], column[second] = column[second], column[first]
+
     def __init__(self):
         self.cells = []
         for column in range (8):
@@ -16,3 +26,4 @@ class Game:
                 Cell (BLUE),
                 Cell (YELLOW)
             ])
+        self.randomize()
